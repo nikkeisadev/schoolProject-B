1c1b25e41b74a3faf0d235c25dc230e51b65402a
ErdeiGergo <144338359+ErdeiGergo@users.noreply.github.com>
2023-11-16 12:53:53 +0100

    Beolvasas0.1

--- C#/AdministrationApp/AdministrationApp/Program.cs
+++ C#/AdministrationApp/AdministrationApp/Program.cs
@@ -6,5 +6,11 @@
         {
 
         }
+
+        static void Beolvasas()
+        {
+            Console.Write("Addj meg egy alkatrészt a paramétereivel együtt ;-vel elválasztva a következő sorrendben: Típusa (ezt lehet rövidítve, vagy azonosítóval is, pl. CPU), Neve, Paraméterek (órajel, memória méretet, háttértár mérete,…), Ára Ft-ban: ");
+            
+        }
     }
 }
